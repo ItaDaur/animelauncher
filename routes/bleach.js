7e59cbafabe25bcc0e316ebd49dbccb16a4d80e3
ItaDaur <96618793+ItaDaur@users.noreply.github.com>
2022-04-23 13:33:07 +0600

    jnjklnlk

---
+++ routes/bleach.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const path = require('path');
+router
+    .route("/")
+    .get((req, res) => res.render(path.resolve('views/bleach.ejs')));
+
+module.exports = router;
---
+++ routes/root.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const https = require("https");
+
+router
+    .route("/")
+    .get((req, res) => res.render(path.resolve('index.ejs')))
+    // .post((req,res) => {
+    //     let name = req.body.animeName;
+    //     let url = "https://anime-facts-rest-api.herokuapp.com/api/v1/" + name;
+    //     https.get(url, function (response) {
+    //         response.on('data', data => {
+    //             let a = JSON.parse(data);
+    //             let mainChar = a.data[0].fact;
+    //             res.render(path.resolve('index.ejs'), {
+    //                 fact:mainChar
+    //             });
+    //         })
+    //     });
+    // });
+module.exports = router;
