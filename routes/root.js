42e379f87b45241e43cd9040270a4b69a91b299b
ItaDaur <96618793+ItaDaur@users.noreply.github.com>
2022-04-23 14:00:46 +0600

    jnjklnlk

--- routes/root.js
+++ routes/root.js
@@ -7,18 +7,5 @@ router
     .route("/")
     .get((req, res) => res.render(path.resolve('index.ejs'), {
         interFact:"Empty"
-    }))
-    // .post((req,res) => {
-    //     let name = req.body.animeName;
-    //     let url = "https://anime-facts-rest-api.herokuapp.com/api/v1/" + name;
-    //     https.get(url, function (response) {
-    //         response.on('data', data => {
-    //             let a = JSON.parse(data);
-    //             let mainChar = a.data[0].fact;
-    //             res.render(path.resolve('index.ejs'), {
-    //                 fact:mainChar
-    //             });
-    //         })
-    //     });
-    // });
+    }));
 module.exports = router;
