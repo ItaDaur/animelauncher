c069a0b9dcfab0aedb1bf2ea206528e6876616d8
ItaDaur <96618793+ItaDaur@users.noreply.github.com>
2022-05-11 04:46:11 +0600

    jnjklnlk

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 if (port == null || port == "") {
-    port = 3001;
+    port = 3002;
 }
 
 app.use(express.static(path.resolve("public")));
@@ -17,6 +17,7 @@ app.use("/", require("./routes/root"));
 app.use("/naruto", require("./routes/naruto"));
 app.use("/blackclover", require("./routes/blackclover"));
 app.use("/logIn", require("./routes/LogIn"));
+app.use("/registration", require("./routes/registration"));
 app.use("/neverland", require("./routes/neverland"));
 app.use("/bleach", require("./routes/bleach"));
 app.use("/demonSlayer", require("./routes/demonSlayer"));
@@ -24,20 +25,6 @@ app.use("/hunter", require("./routes/hunter"));
 app.use("/gintama", require("./routes/gintama"));
 app.use("/heroAcademia", require("./routes/heroAcademia"));
 
-
-app.post("/",((req,res) => {
-    let name = req.body.animeName;
-    let url = "https://anime-facts-rest-api.herokuapp.com/api/v1/" + name;
-    https.get(url, function (response) {
-        response.on('data', data => {
-            let a = JSON.parse(data);
-            let mainChar = a.data[0].fact;
-            res.render(path.resolve("index.ejs"), {
-                interFact:mainChar
-            });
-        })
-    });
-}));
 
 
 app.listen(port, () =>
