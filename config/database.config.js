06be54c815ee566bbfa7b9c43aa6c4f74e388834
ItaDaur <96618793+ItaDaur@users.noreply.github.com>
2022-05-11 11:24:40 +0600

    jnjklnlk

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     //url: 'mongodb://localhost:27017/animeLauncher'
-    url:'mongodb+srv://Daur:123@cluster0.frcvf.mongodb.net/AnimeLauncher?retryWrites=true&w=majority'
+    // url:'mongodb+srv://Daur:123@cluster0.frcvf.mongodb.net/AnimeLauncher?retryWrites=true&w=majority'
+    url:'mongodb+srv://Daur:123@cluster0.lwzh4.mongodb.net/anime?retryWrites=true&w=majority'
 }
